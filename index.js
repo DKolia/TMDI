25668c226b2ec872e5d94d2df287cdddde65e582
David Kolia <Dkolia@icloud.com>
2019-10-10 20:52:31 -0500

    Added Bot emoji, error handling, and Greeting and Goodbye functions

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-const slackbots = require('slackbots');
-const axios = require('axios');
+const Slackbot = require("slackbots");
+const axios = require("axios");
 
-console.log(slackbots);
-console.log(axios);
+// This instantiates the Bot
+const bot = new Slackbot({
+  token: "xoxb-780031740516-782237325153-lqtLVOe20yidCYc1D374iSPM",
+  name: "TMDI Bot"
+});
+
+// This globally sets Params and the Bot's Icon
+const params = {
+  icon_emoji: ":robot_face:"
+};
+
+// This is the Start Handler
+bot.on("start", () => {
+  bot.postMessageToChannel("general", "Get ready for TMDI!", params);
+});
+
+// This is the Error Handler
+bot.on("error", err => console.log(err));
+
+// This is the Message Handler
+bot.on("message", data => {
+  if (data.type !== "message") {
+    return;
+  }
+  handleMessage(data.text);
+});
+
+// Respons to Data
+function handleMessage(message) {
+  if (message === "hello") {
+    bot.postMessageToChannel("general", "Well hello right back!", params);
+  } else if (message === "goodbye") {
+    bot.postMessageToChannel("general", "Byeeeee <3", params);
+  }
+}
+
+console.log("Reaching Line 37");
